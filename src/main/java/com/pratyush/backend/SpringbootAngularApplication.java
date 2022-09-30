3f95431751c28c5e214f64e92841e88374f1a07f
Pratyuxxh <107082402+Pratyuxxh@users.noreply.github.com>
2022-09-30 10:58:01 +0530

    backend-completed

---
+++ src/main/java/com/pratyush/backend/SpringbootAngularApplication.java
@@ -0,0 +1,13 @@
+package com.pratyush.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAngularApplication.class, args);
+	}
+
+}
